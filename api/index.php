784da17cc9de3745f14481eea5ee2cbf757d12fb
Azeggouar <k.azeggouar@gmail.com>
2026-03-19 13:53:15 +0100

    upd

--- api/index.php
+++ api/index.php
@@ -1,4 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Accueil</title>
+</head>
+<body>
+
+<h1>Bienvenue</h1>
+
+<p>Nom : Dev Web</p>
+<p>Email : Devweb@gmail.com</p>
 <?php
-echo "<h1>Bonjour depuis PHP sur Vercel</h1>";
-echo "<p>Déploiement réussi.</p>";
-?> 
+echo "<h1>Hello from Vercel </h1>";
+?>
+<a href="profil.php">Voir mon profil</a>
+
+</body>
+</html>
---
+++ api/profil.php
@@ -0,0 +1,22 @@
+
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Profil</title>
+</head>
+<body>
+
+<h1>Mon Profil</h1>
+
+<ul>
+    <li>Nom : Ali</li>
+    <li>Age : 25</li>
+    <li>Ville : Casablanca</li>
+</ul>
+
+<a href="index.php">Retour à l'accueil</a>
+
+</body>
+</html>
